56b158eea85ce1f9e620fb1250b4baf8296a224c
jonathan <jonathan.dupau24@gmail.com>
2021-12-10 18:18:41 +0100

    add generation of people by random user + add a litlle bit of bootstrap style

--- persona_app/views.py
+++ persona_app/views.py
@@ -33,11 +33,11 @@ def persona_details(request, id):
 def generate_persona(request):
     # API Call
     res = requests.get('https://randomuser.me/api?nat=fr')
+    #  transform to json
     jsonRes = res.json()
-    print('-------------------------------------------')
-    print(res)
-    print('-------------------------------------------')
+    # save results in data
     data = jsonRes['results'][0]
+    # save all data we need
     gender = data['gender']
     first_name = data['name']['first']
     last_name = data['name']['last']
@@ -51,16 +51,38 @@ def generate_persona(request):
     password = data['login']['password']
     age = data['dob']['age']
     picture = data['picture']['thumbnail']
+
+    persona = Persona()
+    persona.first_name = first_name
+    persona.last_name = last_name
+    persona.address_street = address_street
+    persona.address_number = address_number
+    persona.city = city
+    persona.country = country
+    persona.postcode = postcode
+    persona.email = email
+    persona.username = username
+    persona.password = password
+    persona.age = age
+    persona.picture = picture
+
+    persona.save()
+
+    last_persona_id = Persona.objects.latest('id')
+    print('===============================================')
+    print(last_persona_id)
+    print('===============================================')
+
+    context = {
+    'person_details': last_persona_id
+    }
+    return render(request, 'persona_app/persona_details.html', context)
      
-    if res:
-        print('ok mon pote')
-    else:
-        print('pas bon poto')
-        
+    # if res:
+    #     print('ok mon pote')
+    # else:
+    #     print('pas bon poto')
     
-
-
-    return HttpResponse('hola')
 
 
 
