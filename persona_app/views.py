9a9fbd5a8d573cf5e906e82552adbe7d848b7596
jonathan <jonathan.dupau24@gmail.com>
2021-12-10 09:33:11 +0100

    add views and edit installed apps array in settings

--- persona_app/views.py
+++ persona_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+# views to create a new persona
+def task_create(request):
+    return HttpResponse('Creation d\'un persona')
+
+# views to saw detail of a new persona
+def task_details(request, id):
+    return HttpResponse(f'Détails d\'un persona id : {id}')
+
+# views to saw LIST of personas
+def task_list(request):
+    return HttpResponse('Liste des personas') 
