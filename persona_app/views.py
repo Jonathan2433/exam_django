75547530856b8cae407d7f896a64c62bb4e00ee7
jonathan <jonathan.dupau24@gmail.com>
2021-12-10 11:13:08 +0100

    add person_list template + fix views

--- persona_app/views.py
+++ persona_app/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Persona
+
+# views to saw LIST of personas
+def persona_list(request):
+    personas = Persona.objects.all().order_by('-id')
+    context = {
+        'personas':personas
+    }
+    return render(request, 'persona_app/persona_list.html', context)
+
 # views to create a new persona
-def task_create(request):
+def persona_create(request):
     return HttpResponse('Creation d\'un persona')
 
 # views to saw detail of a new persona
-def task_details(request, id):
+def persona_details(request, id):
     return HttpResponse(f'Détails d\'un persona id : {id}')
-
-# views to saw LIST of personas
-def task_list(request):
-    return HttpResponse('Liste des personas') 
