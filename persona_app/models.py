8840df7b5ab99a8da391631583bf193e442c66c8
Mes Morts <titouanescorneboueu@gmail.com>
2021-12-10 10:00:44 +0100

    make migrations and migrate finish

--- persona_app/models.py
+++ persona_app/models.py
@@ -4,16 +4,16 @@ from django.db import models
 class Persona(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=50)
-    address_street = models.CharField()
+    address_street = models.CharField(max_length=50)
     address_number = models.IntegerField()
-    city = models.CharField()
-    country = models.CharField()
-    postcode = models.ImageField()
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=50)
     email = models.EmailField()
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=20)
     age = models.PositiveIntegerField()
-    picture = models.CharField()
+    picture = models.CharField(max_length=50)
 
     def __str__(self):
         return str(self.id), self.first_name, self.last_name, 
